a86e539a01589a388911dda83da039b66e38cc45
uncerso <yayayayaya38@gmail.com>
2021-03-06 13:34:06 +0300

    Added work stealing

--- Kleshchin_Anton/thread_pool/src/thread_pool.cpp
+++ Kleshchin_Anton/thread_pool/src/thread_pool.cpp
@@ -1,16 +1,22 @@
 #include "thread_pool.hpp"
 
-ThreadPool::ThreadPool(size_t size) {
+ThreadPool::ThreadPool(size_t size) 
+    : local_task_queues(size)
+{
     for (size_t i = 0; i < size; ++i)
-        threads.emplace_back(*this);
+        threads.emplace_back(*this, i);
 }
 
-void ThreadPool::Enqueue(Func && task) {
-    task_queue.Push(std::move(task));
+void ThreadPool::Enqueue(Func && task, size_t thread_queue_id) {
+    if (thread_queue_id == -1)
+        task_queue.Push(move(task));
+    else
+        local_task_queues[thread_queue_id].Push(move(task));
 }
 
-ThreadPool::SThread::SThread(ThreadPool & pool)
-    : pool(pool)
+ThreadPool::SThread::SThread(ThreadPool & pool, size_t id)
+    : id(id)
+    , pool(pool)
     , stop(false)
     , thread(std::ref(*this))
 {}
@@ -25,19 +31,31 @@ void ThreadPool::SThread::operator()() noexcept {
         if (stop)
             return;
 
-        pool.RunPendingTask();
+        pool.RunPendingTask(id);
     }
 }
 
-void ThreadPool::RunPendingTask() noexcept {
+void ThreadPool::RunPendingTask(size_t id) noexcept {
     Func foo;
-    if (!task_queue.TryPop(foo)) {
+    if (!local_task_queues[id].TryPop(foo) &&
+        !task_queue.TryPop(foo) && 
+        !TrySteal(foo, id))
+    {
         // Just let other threads/processes do work.
         // Rewrite this line with conditional_variables if threads should sleep when task_queue is empty.
         std::this_thread::yield();
     } else {
         (*foo)();
         for (auto & conts : foo->continuations)
-            Enqueue(move(conts));
+            Enqueue(move(conts), id);
     }
+}
+
+bool ThreadPool::TrySteal(Func & res, size_t current_id) noexcept {
+    const auto n = local_task_queues.size();
+    for (size_t i = 1; i < n; ++i) {
+        if (local_task_queues[(current_id + i) % n].TryPop(res))
+            return true;
+    }
+    return false;
 }
--- Kleshchin_Anton/thread_pool/src/thread_pool.hpp
+++ Kleshchin_Anton/thread_pool/src/thread_pool.hpp
@@ -8,23 +8,29 @@
 
 class ThreadPool {
     using Func = std::unique_ptr<Callable>;
+    using TaskQueue = TSQueue<Func>;
+
     class SThread {
+        size_t id;
         ThreadPool & pool;
         std::atomic<bool> stop;
         std::thread thread;
-
     public:
-        SThread(ThreadPool & pool);
+        SThread(ThreadPool & pool, size_t id);
         ~SThread();
         void operator()() noexcept;
     };
     friend class SThread;
 
     std::list<SThread> threads;
-    TSQueue<Func> task_queue;
+    TaskQueue task_queue;
+    std::vector<TaskQueue> local_task_queues;
 public:
     ThreadPool(size_t size);
 
-    void Enqueue(Func && task);
-    void RunPendingTask() noexcept;
+    void Enqueue(Func && task, size_t thread_queue_id = -1);
+    void RunPendingTask(size_t id = 0) noexcept;
+
+private:
+    bool TrySteal(Func & res, size_t current_id) noexcept;
 };
